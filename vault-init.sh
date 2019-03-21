0e7b9c1f0316c5b3b7b8f4bcfc7e2e5f114106cd
aleksey <lysakaleksey@gmail.com>
2019-03-21 23:27:27 +0200

    Vault initialization

--- vault-init.sh
+++ vault-init.sh
@@ -6,24 +6,24 @@ echo ""
 echo "Initializing vault"
 vault operator init > vault-init.keys
 
-#echo ""
-#echo "Unlocking vault by using 3 keys"
-#vault operator unseal $(sed -n '1p' < vault-init.keys | awk '{print $4}')
-#vault operator unseal $(sed -n '2p' < vault-init.keys | awk '{print $4}')
-#vault operator unseal $(sed -n '3p' < vault-init.keys | awk '{print $4}')
-#
-#echo ""
-#echo "Vault login using root token"
-#vault login $(sed -n '7p' < vault-init.keys | awk '{print $4}')
-#
-#echo ""
-#echo "Setup initial secrets"
-#vault write secret/${SECRET_NAME} @secrets.json
-#
-#echo ""
-#echo "Trying to read secrets we just loaded"
-#vault read --format=json secret/${SECRET_NAME}
-#
-#echo "Done!"
-#
-#echo "Login to ui and check. Make sure you copied vault-init.keys!"
+echo ""
+echo "Unlocking vault by using 3 keys"
+vault operator unseal "$(sed -n 1p < vault-init.keys | awk '{print $4}')"
+vault operator unseal "$(sed -n 2p < vault-init.keys | awk '{print $4}')"
+vault operator unseal "$(sed -n 3p < vault-init.keys | awk '{print $4}')"
+
+echo ""
+echo "Vault login using root token"
+vault login "$(sed -n 7p < vault-init.keys | awk '{print $4}')"
+
+echo ""
+echo "Setup initial secrets"
+vault write secret/${SECRET_NAME} @secrets.json
+
+echo ""
+echo "Trying to read secrets we just loaded"
+vault read --format=json secret/${SECRET_NAME}
+
+echo "Done!"
+
+echo "Login to ui and check. Make sure you copied vault-init.keys!"
