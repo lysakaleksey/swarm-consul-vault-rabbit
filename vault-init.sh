f2512f682f1016223f7d80d6d5fc93b65ed0d460
aleksey <lysakaleksey@gmail.com>
2019-03-21 23:18:00 +0200

    Vault initialization

--- vault-init.sh
+++ vault-init.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env bash
 
-export VAULT_ADDR=http://192.168.99.120:8200
+source main.env
 
+echo ""
+echo "Initializing vault"
 vault operator init > vault-init.keys
 
-vault login s.jBdjb7PZzYaMeVkpZuBGhlN5
-
-vault write secret/test @secrets.json
+#echo ""
+#echo "Unlocking vault by using 3 keys"
+#vault operator unseal $(sed -n '1p' < vault-init.keys | awk '{print $4}')
+#vault operator unseal $(sed -n '2p' < vault-init.keys | awk '{print $4}')
+#vault operator unseal $(sed -n '3p' < vault-init.keys | awk '{print $4}')
+#
+#echo ""
+#echo "Vault login using root token"
+#vault login $(sed -n '7p' < vault-init.keys | awk '{print $4}')
+#
+#echo ""
+#echo "Setup initial secrets"
+#vault write secret/${SECRET_NAME} @secrets.json
+#
+#echo ""
+#echo "Trying to read secrets we just loaded"
+#vault read --format=json secret/${SECRET_NAME}
+#
+#echo "Done!"
+#
+#echo "Login to ui and check. Make sure you copied vault-init.keys!"
