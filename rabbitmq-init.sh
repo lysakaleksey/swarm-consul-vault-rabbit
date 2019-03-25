e50dac8ae078e8646b0fa362625e6c6e987ffb5e
aleksey <lysakaleksey@gmail.com>
2019-03-25 08:06:55 +0200

    ha mode policy

--- rabbitmq-init.sh
+++ rabbitmq-init.sh
@@ -5,10 +5,20 @@ source rabbitmq.env
 
 RABBITMQ_HOST="$(docker-machine ip $NODE1)"
 
-DATA='{"pattern":"^ha\\.","definition":{"ha-mode":"all"}}'
+HA_POLICY=`cat<<EOF
+{
+    "pattern": "^ha\\.",
+    "definition": {
+        "ha-mode": "all",
+        "expires": 1800000
+    },
+    "priority": 1,
+    "apply-to": "queues"
+}
+EOF`
 
-curl -X PUT -u "$RABBITMQ_DEFAULT_USER:$RABBITMQ_DEFAULT_PASS" \
--H "Content-Type: application/json" "http://${RABBITMQ_HOST}:15672/api/policies/%2f/ha-all" -d "${DATA}"
+curl -s -X PUT -u "$RABBITMQ_DEFAULT_USER:$RABBITMQ_DEFAULT_PASS" -H "Content-Type: application/json" \
+  http://${RABBITMQ_HOST}:15672/api/policies/%2f/ha-all -d "${HA_POLICY}"
 
 ##
 #python rabbitmqadmin -H $RABBITMQ_HOST -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS list exchanges
@@ -20,5 +30,5 @@ curl -X PUT -u "$RABBITMQ_DEFAULT_USER:$RABBITMQ_DEFAULT_PASS" \
 #
 ##rabbitmqadmin declare binding source="events" destination="notifications" routing_key="notifications.v1" destination_type="queue"
 #
-#rabbitmqadmin declare queue name=uid-440099 durable=false auto-delete=true \
+#rabbitmqadmin declare queue name=uid-440099 durable=true auto-delete=false \
 #     'arguments={"x-message-ttl":86400000,"x-expires":86400000,"x-dead-letter-exchange":"deadletters.fanout"}'
