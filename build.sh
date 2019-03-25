d6bf1e8e2e43a128b8d83766f09fcfce1c629a63
aleksey <lysakaleksey@gmail.com>
2019-03-25 09:31:01 +0200

    Regular commit

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#docker image build -t lysakaleksey/rabbitmq:3.7-management-alpine ./rabbitmq
-#docker push lysakaleksey/rabbitmq:3.7-management-alpine
+docker image build -t lysakaleksey/rabbitmq:3.7-management-alpine ./rabbitmq
+docker push lysakaleksey/rabbitmq:3.7-management-alpine
 
-#docker image build -t lysakaleksey/haproxy-for-rabbitmq:1.9-alpine ./haproxy
-#docker push lysakaleksey/haproxy-for-rabbitmq:1.9-alpine
+docker image build -t lysakaleksey/haproxy-for-rabbitmq:1.9-alpine ./haproxy
+docker push lysakaleksey/haproxy-for-rabbitmq:1.9-alpine
 
 docker image build -t lysakaleksey/rabbitmq-app:latest ./testapp
 docker push lysakaleksey/rabbitmq-app:latest
